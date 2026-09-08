2812261e1815411bc8f904430d5c469bd5081d5a
oldgevdev <neocitieshtmlcssjavascript@gmail.com>
2026-09-08 19:07:48 +0600

    Create Logo.java

---
+++ Logo.java
@@ -0,0 +1,16 @@
+// Файл: Logo.java
+public class Logo {
+    public static void drawLogo() {
+        System.out.println("========================================");
+        System.out.println("  [GREEN LOGO]  ⚡ BLOXIK 2005 ⚡   ");
+        System.out.println("========================================");
+        System.out.println("        ____  _     _______   _______ ");
+        System.out.println("       | __ )| |   |  _  \\ \\ / /_   _|");
+        System.out.println("       |  _ \\| |   | | | |\\ V /  | |  ");
+        System.out.println("       | |_) | |___| |_| |/   \\  | |  ");
+        System.out.println("       |____/|_____|_____/_/ \\_\\ |_|  ");
+        System.out.println("========================================");
+        System.out.println("   Добро пожаловать в ROBLOX 2005!     ");
+        System.out.println("========================================");
+    }
+}
